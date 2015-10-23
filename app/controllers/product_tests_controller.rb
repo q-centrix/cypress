5465dd8f6f355c163488117dfd6d49ff294ca571
jaebird123 <jeaton@mitre.org>
2015-10-23 13:24:03 -0400

    For the parent to child chain: Vendor - Product - Product Test - Task - Test Execution, Added automatic status change for ancestors whenever child objects are edited / added / destroyed. The statuses along with groups of passing, failing, and incomplete children are recorded in the rails cache. Added testing for these cached items.

--- app/controllers/product_tests_controller.rb
+++ app/controllers/product_tests_controller.rb
@@ -1,9 +1,41 @@
 class ProductTestsController < ApplicationController
+  before_action :set_vendor_and_product, only: [:new, :create]
 
   def new
-    @product = Product.find(params[:product_id])
-    @vendor = @product.vendor
     @product_test = @product.product_tests.build
   end
 
+  def create
+    @product_test = ProductTest.new(product_test_params)
+    @product_test.product = @product
+    @product_test.name = Measure.top_level.where(hqmf_id: @product_test.measure_id).first.name
+    # v v v DO NOT USE IN FINAL VERSION v v v #
+    @product_test.bundle_id = Bundle.first.id
+    @product_test.effective_date = 1
+    # ^ ^ ^ DO NOT USE IN FINAL VERSION ^ ^ ^ #
+    @product_test.save!
+    flash_product_test_comment(@product_test, 'success', 'created')
+    respond_to do |f|
+      f.json {} # <-- must be fixed later
+      f.html { redirect_to vendor_product_path(@vendor, @product) }
+    end
+  rescue Mongoid::Errors::Validations
+    render :new
+  end
+
+  private
+
+  def set_vendor_and_product
+    @product = Product.find(params[:product_id])
+    @vendor = @product.vendor
+  end
+
+  def product_test_params
+    params[:product_test].permit(:name, :measure_id, :state)
+  end
+
+  def flash_product_test_comment(product_test_name, notice_type, action_type)
+    flash[:notice] = "Product Test '#{product_test_name}' was #{action_type}."
+    flash[:notice_type] = notice_type
+  end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,8 +10,12 @@ Rails.application.configure do
   config.eager_load = false
 
   # Show full error reports and disable caching.
+  #   Caching temporarily enabled to test caching ~ JaeBird
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
+
+  #   Caching temporarily enabled to test caching ~ JaeBird
+  config.cache_store = :memory_store, { size: 64.megabytes }
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
@@ -25,7 +29,7 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true
+  config.assets.debug = false
 
   config.assets.compress = true
 
