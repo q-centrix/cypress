425ea028d24427a53078de703a88814a9242396a
Cristen Jones <cristen@outlook.com>
2015-10-30 12:34:34 -0400

    revert removed certification test types from products

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,18 +10,29 @@ class Product
   field :name, type: String
   field :version, type: String
   field :description, type: String
-  field :ehr_type, type: String
-  # state should be 'passing', 'failing', etc.
-  field :state, type: String, default: 'incomplete'
 
   validates :name, presence: true,
                    uniqueness: { :scope => :vendor,
                                  :message => 'Product name was already taken. Please choose another.' }
   validates :ehr_type, presence: true, inclusion: { in: %w(provider hospital) }
+  validate :at_least_one_test_type?
   validates :vendor, presence: true
   validates :state, inclusion: { in: %w(passing failing incomplete) }
+
+  field :ehr_type, type: String
+  field :c1_test, type: Boolean
+  field :c2_test, type: Boolean
+  field :c3_test, type: Boolean
+  field :c4_test, type: Boolean
+
+  # state should be 'passing', 'failing', etc.
+  field :state, type: String
 
   scope :passing, -> { where(state: 'passing') }
   scope :failing, -> { where(state: 'failing') }
   scope :incomplete, -> { where(state: 'incomplete') }
+
+  def at_least_one_test_type?
+    errors.add(:tests, 'Product must include at least one certification test.') unless [c1_test, c2_test, c3_test, c4_test].any? { |is_true| is_true }
+  end
 end
