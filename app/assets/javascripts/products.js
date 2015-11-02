8d1f44bc653394a8aec7130b132ffcab30bbcbc8
Cristen Jones <cristen@outlook.com>
2015-11-02 18:55:53 -0500

    refactoring for product and vendor forms

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,2 +1,24 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+var ready;
+ready = function() {
+
+
+  $('.btn-checkbox input:disabled').parent().addClass('disabled');
+  $('.btn-checkbox input:checked:not(:disabled)').parent().addClass('active');
+
+  $('.btn-checkbox input:checkbox').on('change', function() {
+    $(this).parent().toggleClass('active', $(this).prop('checked'));
+  });
+
+  $('.btn-checkbox input:radio').on('change', function() {
+    $('input:radio[name="'+$(this).attr('name')+'"]').parent().removeClass('active');
+    $(this).parent().addClass('active');
+  });
+
+
+};
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
